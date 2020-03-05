307c675bc9f5b2b5dc9d149b89481e422462772c
sheyda_masoudi <ubuntuic6449@gmail.com>
2020-03-05 21:36:38 +0330

    read line to line

--- piano.py
+++ piano.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  5 19:07:02 2020
 
+@author: sheyda
+"""
+s=input("please insert your file ")
+file=open(s,"r")
+c=[ch for ch in file.read()]
+for char in c:
+    print(char)
